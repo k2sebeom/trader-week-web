f0445b92fef017e3905738f15dc69d521db0de05
k2sebeom <moses97@gmail.com>
2024-08-14 01:55:50 +0900

    feat: Add get me api

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,21 +1,66 @@
 import React, { useEffect, useState } from 'react';
 import '../App.css';
+import '../components/button.css';
 
 import LogoImg from '../assets/images/Logo.png';
-import { getAllRooms } from '../utils/api';
-import { RoomDTO } from '../types/dto';
+import { getAllRooms, getMe, signIn } from '../utils/api';
+import { RoomDTO, UserDTO } from '../types/dto';
 import RoomTable from '../components/RoomTable';
+import Swal from 'sweetalert2';
 
 function Home() {
   const [rooms, setRooms] = useState<RoomDTO[]>([]);
+  const [me, setMe] = useState<UserDTO | null>(null);
 
   useEffect(() => {
     getAllRooms().then((data) => setRooms(data));
-  });
+  }, []);
+
+  useEffect(() => {
+    getMe().then((data) => setMe(data));
+  }, []);
+
   return (
     <div className="container">
       <img alt="logo" src={LogoImg} className="logo" />
-      <h1>Fuck you</h1>
+      <h1>Traders Week</h1>
+      <div
+        style={{
+          marginBottom: 20,
+        }}
+      >
+        {me === null ? (
+          <button
+            className="styled-button"
+            onClick={async () => {
+              const result = await Swal.fire({
+                title: 'Sign in / up as a trader!',
+                html: `
+                <form>
+              <input id="nickname" placeholder="nickname" class="swal2-input" />
+              <input id="password" autocomplete placeholder="password" type="password" class="swal2-input" />
+              </form>
+            `,
+                confirmButtonText: 'Start Trading!',
+                confirmButtonColor: '#007f65',
+                preConfirm: () => {
+                  return [document.getElementById('nickname'), document.getElementById('password')];
+                },
+              });
+              const [nickname, password] = result.value;
+              const user = await signIn(nickname.value, password.value);
+              if (user !== null) {
+                setMe(user);
+              }
+            }}
+          >
+            Join the Market!
+          </button>
+        ) : (
+          <h1>{me.nickname}</h1>
+        )}
+      </div>
+
       <RoomTable rooms={rooms} />
     </div>
   );
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,11 +1,34 @@
 import axios from 'axios';
 import config from './configs';
+import { UserDTO } from '../types/dto';
 
 const instance = axios.create({
   baseURL: config.api_base,
+  withCredentials: true,
 });
 
 export async function getAllRooms() {
   const resp = await instance.get('/api/game/');
   return resp.data;
+}
+
+export async function getMe(): Promise<UserDTO | null> {
+  try {
+    const resp = await instance.get('/api/user/me');
+    return resp.data;
+  } catch {
+    return null;
+  }
+}
+
+export async function signIn(nickname: string, password: string): Promise<UserDTO | null> {
+  try {
+    const resp = await instance.post('/api/user/signin', {
+      nickname,
+      password,
+    });
+    return resp.data;
+  } catch {
+    return null;
+  }
 }
