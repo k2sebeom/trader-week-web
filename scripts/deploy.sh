944e50b7600345893439bcfd2c7e6fb11d43caa7
k2sebeom <moses97@gmail.com>
2024-08-24 23:02:21 +0900

    feat: Add deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+REMOTE_TARGET=$1
+BUILD_DIR=dist
+TAR_FILE="trader_web.tar.gz"
+
+# Build Artifacts
+rm -r $BUILD_DIR
+yarn build
+tar cvzf $TAR_FILE $BUILD_DIR
+
+scp $TAR_FILE $REMOTE_TARGET:.
+rm $TAR_FILE
+
+ssh $REMOTE_TARGET << EOF
+    tar xvzf $TAR_FILE
+    sudo rm -rf /usr/share/nginx/html
+    sudo mv dist /usr/share/nginx/html
+EOF
